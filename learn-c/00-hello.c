8f31c7cde935788948373c48d312adc24ff55200
chris <caaker.0@gmail.com>
2025-09-09 20:34:02 -0500

    auto-commit

--- learn-c/00-hello.c
+++ learn-c/00-hello.c
@@ -3,16 +3,18 @@
 
 int main() {
 
-  // char * string1;
+  // hello universe
+  printf("Hello Universe. Please enter up to 4 characters. \n");
 
   // reads up to 4 characters
   // scanf automatiaclly adds the null character - \0
-  // if the user puts in more than 4 character buffer overflow occurs
+  // if the user puts in more than 4 characters, a buffer overflow occurs, but no error is thrown
   char string2[5];
   scanf("%4s", string2);
-  
-  printf("Hello Universe \n");
 
+  // mirror back the user input
+  printf("You entered: %s\n", string2);
+  
   // return 0 on success
   // c does not have keywords for error handling, but we can check function return values
   return 0;
--- learn-c/00-style.c
+++ learn-c/00-style.c
@@ -1,4 +1,5 @@
 /*
+
 Style Guide
 
 Use same as C++
