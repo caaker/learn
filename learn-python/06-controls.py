d34dd73a589f75b44f7fb077a33c2cca1a7512c0
caaker <caaker.0@gmail.com>
2026-02-23 22:19:14 +0000

    codespace commit

--- learn-python/06-controls.py
+++ learn-python/06-controls.py
@@ -4,19 +4,26 @@ print("controls - loops")
 print("***************")
 print("")
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers = [0, 1, 2, 3]
 
-# for
+# prints 0, 1, 2, 3
 for number in numbers:
   print(number)
 
-# while
+# prints 0, 1, 2, 3
+# if we don't need the a, by convention we can replace it with _
+for a in range(4):
+  print(a)
+
+act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
+
+# prints 0, 1, 2, 3
 i = 0
-while i < 3:
+while i <= 3:
   print(i)
   i += 1
 
-# if / elif / else
+# prints Positive
 x = 10
 if x > 0:
   print("Positive")
---
+++ learn-python/07-math.py
@@ -0,0 +1,15 @@
+print("")
+print("***************")
+print("math")
+print("***************")
+print("")
+
+import random
+
+# seeding will cause random.uniform to always produce the same value
+# random.seed(42)
+print(random.uniform(-1,1))
+
+# sums up an iterable with an optional default value
+# output is 5.0
+sum([1.5, 2.5], 1)
